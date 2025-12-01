383e4603c1067d72a1601949024f3c26b8e090ee
Albert Wang <ru04jo30801@gmail.com>
2025-12-01 16:56:14 +0800

    Swap network interface assignments for IP settings

--- ip-set.sh
+++ ip-set.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # 要設定的 IP 與 netmask
+# Shared
 IP1="192.168.81.1"
 NETMASK1="255.255.255.0"
 
+# Host-Only
 IP2="192.168.94.1"
 NETMASK2="255.255.255.0"
 
 # 目標網卡（你輸出的 ifconfig 顯示 bridge101、bridge100）
-IFACE1="bridge101"
-IFACE2="bridge100"
+IFACE1="bridge100"
+IFACE2="bridge101"
 
 echo "Setting $IFACE1 to $IP1 ..."
 sudo ifconfig $IFACE1 inet $IP1 netmask $NETMASK1
